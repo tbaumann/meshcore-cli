b67a127331b527cc468dad26236531d7b13569c2
Florent de Lamotte <florent@frizoncorrea.fr>
2025-03-24 16:48:03 +0100

    meshcore lib moved to another project

--- meshcore/__init__.py
+++
@@ -1,5 +0,0 @@
-from meshcore.mclib import printerr
-from meshcore.mclib import MeshCore
-from meshcore.mclib import TCPConnection
-from meshcore.mclib import BLEConnection
-from meshcore.mclib import SerialConnection
