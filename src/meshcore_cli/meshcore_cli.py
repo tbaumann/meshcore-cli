a18ac033b27dbc1b4e3e9150346f7a293501aa2d
Florent <florent@frizoncorrea.fr>
2025-07-21 19:25:42 +0200

    v1.1.8 depends on meshcore_py v2.0.1

--- src/meshcore_cli/meshcore_cli.py
+++ src/meshcore_cli/meshcore_cli.py
@@ -23,7 +23,7 @@ from prompt_toolkit.shortcuts import radiolist_dialog
 from meshcore import MeshCore, EventType, logger
 
 # Version
-VERSION = "v1.1.7"
+VERSION = "v1.1.8"
 
 # default ble address is stored in a config file
 MCCLI_CONFIG_DIR = str(Path.home()) + "/.config/meshcore/"
